c8b81ef49b5cbf8356a56e337f6bd0681b0ceab2
SuchitaBante <bantesuchi321@gmail.com>
2026-06-16 15:56:13 +0530

    Applet Tech. Service Homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/hero/hero.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss']
+})
+export class HeroComponent {
+  constructor(private router: Router) {}
+
+navigateToContact() {
+  this.router.navigate(['/contact']);
+}
+}
